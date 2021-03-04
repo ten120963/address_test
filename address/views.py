197e75ebc33e5b82f5a842624cef168a2e8ad1ed
Thomas Nowak <t.nowak@roadrunner.com>
2021-03-04 11:56:34 -0500

    modified data display

--- address/views.py
+++ address/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from .models import Contact
 
 def home(request):
-	return render(request, 'home.html', {})
+	all_contacts = Contact.objects.all
+	return render(request, 'home.html', {'all_contacts': all_contacts})
 
 def add(request):
 	return render(request, 'add.html', {})	
