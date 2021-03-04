2ca22dcf81aa249629bc92fabf5d60ac9a5ac571
Thomas Nowak <t.nowak@roadrunner.com>
2021-03-04 12:52:54 -0500

    added pages

--- address/views.py
+++ address/views.py
@@ -1,10 +1,47 @@
 from django.shortcuts import render, redirect
 from .models import Contact
+from .forms import ContactForm
+from django.contrib import messages
 
 def home(request):
 	all_contacts = Contact.objects.all
 	return render(request, 'home.html', {'all_contacts': all_contacts})
 
 def add(request):
-	return render(request, 'add.html', {})	
+	if request.method == "POST":
+		form = ContactForm(request.POST or None)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Contact has been added!'))
+			return redirect('home')		
+		else:
+			messages.success(request, ('Error.  Please try again.'))	
+			return render(request, 'add.html', {})	
+	else:
+		return render(request, 'add.html', {})	
+
+def edit(request, list_id):
+	if request.method == "POST":
+		current_contact = Contact.objects.get(pk=list_id)
+		form = ContactForm(request.POST or None, instance=current_contact)
+		if form.is_valid():
+			form.save()
+			messages.success(request, ('Contact Has Been Edited!'))
+			return redirect('home')
+		else:
+			messages.success(request, ('Seems Like There Was An Error...'))	
+			return render(request, 'edit.html', {})	
+	else:
+		get_contact = Contact.objects.get(pk=list_id)
+		return render(request, 'edit.html', {'get_contact': get_contact})	
+
+def delete(request, list_id):
+	if request.method == "POST":
+		current_contact = Contact.objects.get(pk=list_id)
+		current_contact.delete()
+		messages.success(request, ('Contact Has Been Deleted!'))
+		return redirect('home')
+	else:
+		messages.success(request, ('Nothing To See Here...'))	
+		return redirect('home')			
 
